28bb1f8a52f12fc1ffb148d92b4ece23a1f0685b
Kuro <iaguinhodosantos2@gmail.com>
2022-12-02 15:58:55 -0300

    Cena Renata Coisinhas do pedrinho

--- EscapeEmocional/Assets/Scripts/Utilidades/Rotator.cs
+++ EscapeEmocional/Assets/Scripts/Utilidades/Rotator.cs
@@ -8,15 +8,12 @@ public class Rotator : MonoBehaviour
     [SerializeField] Vector3 rotation;
     public float speedRotation;
     public float speedPingPong;
-    public Transform p1;
-    public Transform p2;
-
+    
     float t;
 
     void FixedUpdate()
     {
         transform.Rotate(rotation * speedRotation * Time.deltaTime);
-        t = Mathf.PingPong(Time.time * speedPingPong, 1.0f);
-        transform.position = Vector3.Lerp(p1.position, p2.position, t);
+       
     }
 }
